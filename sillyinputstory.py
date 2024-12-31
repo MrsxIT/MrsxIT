3ebff2ded66b846e93efc1ad6a1d442d96c91b2f
MrsxIT <jasmine@jjfab.xyz>
2024-12-31 18:49:34 +0000

    Create sillyinputstory.py

---
+++ sillyinputstory.py
@@ -0,0 +1,10 @@
+# This is a silly story to practice input with - add your own input and see what story comes out!
+
+name = ""
+adjective = ""
+favourite_snack = ""
+number = ""
+type_of_tree = ""
+
+silly_story = f"Hi, my name is {name}. I really like {adjective} {favourite_snack}, it tastes so good! I like to eat it so much that I eat {number} of it every single day. My favourite place to eat {favourite_snack} is under a {type_of_tree}!"
+print(silly_story)
